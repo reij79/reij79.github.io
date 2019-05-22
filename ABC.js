b7880ce9a86f3a662f2d08a6e96d5c0ffbf3dc2e
reij79 <38755828+reij79@users.noreply.github.com>
2019-05-22 23:21:58 +1000

    Add files via upload

---
+++ ABC.js
@@ -0,0 +1,54 @@
+    
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+    var cols = [{
+        id: "name",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "streetaddress",
+        alias: "address",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "location",
+        dataType: tableau.dataTypeEnum.geometry
+    }];
+
+    var tableSchema = {
+        id: "ABC",
+        alias: "ABC",
+        columns: cols
+    };
+
+    schemaCallback([tableSchema]);
+    };
+
+    myConnector.getData = function(table, doneCallback) {
+    $.getJSON("https://data.gov.au/geoserver/abc-local-stations/wfs?request=GetFeature&typeName=ckan_d534c0e9_a9bf_487b_ac8f_b7877a09d162&outputFormat=json", function(resp) {
+        var feat = resp.features,
+            tableData = [];
+
+        // Iterate over the JSON object
+        for (var i = 0, len = feat.length; i < len; i++) {
+            tableData.push({
+                "name": feat[i].properties.name,
+                "streetaddress": feat[i].properties.streetaddress,
+                "location": feat[i].geometry
+            });
+        }
+
+        table.appendRows(tableData);
+        doneCallback();
+    });
+};
+
+    tableau.registerConnector(myConnector);
+})();
+
+$(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "USGS Earthquake Feed";
+        tableau.submit();
+    });
+});
