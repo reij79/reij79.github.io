0f7703d7196aae1e188fdfa30cc42a7694749f92
reij79 <38755828+reij79@users.noreply.github.com>
2019-05-23 01:27:14 +1000

    Update Roads.js

--- Roads.js
+++ Roads.js
@@ -86,6 +86,7 @@
 
     function setHeader(xhr) {
         xhr.setRequestHeader('Authorization', 'apikey o10JorpdwvgF9yVK6TZVmLnahdB4UtNz1dO1');
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
         }
 
 };
