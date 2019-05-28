9ea6ecc19293dd57b819b25e9cc7df97067a06c0
reij79 <38755828+reij79@users.noreply.github.com>
2019-05-29 00:51:20 +1000

    Add files via upload

---
+++ RoadsV2.js
@@ -0,0 +1,108 @@
+    
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+    var cols = [{
+        id: "station_id",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "name",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "road_functional_hierarchy",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "lane_count",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "road_classification_type",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "road_classification_admin",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "lga",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "suburb",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "post_code",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "wgs84_latitude",
+        alias: "latitude",
+        dataType: tableau.dataTypeEnum.geometry
+    }, {
+        id: "wgs84_longitude",
+        alias: "longitude",
+        dataType: tableau.dataTypeEnum.geometry
+    }];
+
+    var tableSchema = {
+        id: "road_traffic_counts_station_reference",
+        alias: "Station Reference",
+        columns: cols
+    };
+
+    schemaCallback([tableSchema]);
+    };
+
+
+    myConnector.getData = function(table, doneCallback) {
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.transport.nsw.gov.au/v1/roads/spatial?format=geojson&q=select%20%2A%20from%20road_traffic_counts_station_reference%0A",
+            "method": "GET",
+            "headers": {
+              "Authorization": "apikey o10JorpdwvgF9yVK6TZVmLnahdB4UtNz1dO1",
+              "cache-control": "no-cache"
+            }
+          }
+
+          //$.ajax(settings).done(function (response) {
+          //  console.log(response);
+          //});
+          $.ajax(settings).done({
+            success: function(resp) {
+            var feat = resp.features,
+            tableData = [];
+
+        // Iterate over the JSON object
+        for (var i = 0, len = feat.length; i < len; i++) {
+            tableData.push({
+                "station_id": feat[i].properties.station_id,
+                "name": feat[i].properties.name,
+                "road_functional_hierarchy": feat[i].properties.road_functional_hierarchy,
+                "lane_count": feat[i].properties.lane_count,
+                "road_classification_type": feat[i].properties.road_classification_type,
+                "road_classification_admin": feat[i].properties.road_classification_admin,
+                "lga": feat[i].properties.lga,
+                "suburb": feat[i].properties.suburb,
+                "post_code": feat[i].properties.post_code,
+                "wgs84_latitude": feat[i].properties.wgs84_latitude,
+                "wgs84_longitude": feat[i].properties.wgs84_longitude
+            });
+        }
+
+        table.appendRows(tableData);
+        doneCallback();
+    },
+
+    });
+
+};
+
+
+
+    tableau.registerConnector(myConnector);
+})();
+
+$(document).ready(function () {
+    $("#submitButton").click(function () {
+        tableau.connectionName = "road_traffic_counts_station_reference";
+        tableau.submit();
+    });
+});
